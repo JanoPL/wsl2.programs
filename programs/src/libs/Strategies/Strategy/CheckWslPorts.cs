f45dc7d14c0cc6d7d5da7136c09d7c5b8e07f8c9
JanoPL <xjanusz4@gmail.com>
2022-12-30 05:27:19 +0100

    clean usings

--- programs/src/libs/Strategies/Strategy/CheckWslPorts.cs
+++ programs/src/libs/Strategies/Strategy/CheckWslPorts.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using WSL;
 
 namespace Strategies.Strategy
@@ -13,7 +8,7 @@ namespace Strategies.Strategy
         private ILogger _logger;
         private IWsl _wsl;
 
-        public CheckWslPorts(ILogger logger, IWsl wsl) 
+        public CheckWslPorts(ILogger logger, IWsl wsl)
         {
             _logger = logger;
             _wsl = wsl;
